043e0b89d458fedcafe28ee0e028d9c8b6b980cc
Emmanuel Okoro -267666 <emmanuelokoro@tuni.fi>
2020-07-27 11:05:15 +0300

    more compression

--- tipcalculator.js
+++ tipcalculator.js
@@ -3,14 +3,21 @@ function calculateTip() {
   var serviceQual = document.getElementById("serviceQual").value;
   var numOfPeople = document.getElementById("peopleamt").value;
 
+
   if (billAmt === "" || serviceQual == 0) {
     alert("Please enter values");
     return;
   }
+
+
   if (numOfPeople === "" || numOfPeople <= 1) {
     numOfPeople = 1;
     document.getElementById("each").style.display = "none";
-  } else {
+
+  } 
+  
+  else 
+  {
     document.getElementById("each").style.display = "block";
   }
 
@@ -24,6 +31,7 @@ function calculateTip() {
 
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
+
 
 document.getElementById("calculate").onclick = function() {
   calculateTip();
